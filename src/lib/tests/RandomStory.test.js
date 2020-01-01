266e16d0e8fc4d62225231fc7cc8c323b618f6c0
Jeremie Pardou <571533+jrmi@users.noreply.github.com>
2020-01-01 10:44:11 +0100

    Add side effect param and change way condition are working

--- src/lib/tests/RandomStory.test.js
+++ src/lib/tests/RandomStory.test.js
@@ -1,106 +1,147 @@
-import RandomStory from "../RandomStory";
+import RandomStory from '../RandomStory';
 
-it("Instanciate without crashing", () => {
+it('Instanciate without crashing', () => {
   new RandomStory();
 });
 
-it("I can add a new value", () => {
+it('I can add a new value', () => {
   const rs = new RandomStory();
-  rs.add("start", "test");
+  rs.add({ domain: 'start', label: 'test' });
 
-  expect(rs.domains.start).toEqual([
-    {
-      additions: [],
-      conditions: [],
-      label: "test",
-      vars: {},
-      weight: 1
-    }
-  ]);
+  expect(rs.domains.start.length).toEqual(1);
 });
 
-it("Total weigth is working well", () => {
+it('Is total weight working ?', () => {
   const rs = new RandomStory();
 
-  rs.add("start", "test");
-  expect(rs.total_weight(rs.domains["start"])).toBeCloseTo(1);
+  rs.add({ domain: 'start', label: 'test' });
+  expect(rs.totalWeight(rs.domains['start'])).toBeCloseTo(1);
 
-  rs.add("start", "end");
-  expect(rs.total_weight(rs.domains["start"])).toBeCloseTo(2);
+  rs.add({ domain: 'start', label: 'end' });
+  expect(rs.totalWeight(rs.domains['start'])).toBeCloseTo(2);
 
-  rs.add("start", "top", 0.2);
-  expect(rs.total_weight(rs.domains["start"])).toBeCloseTo(2.2);
+  rs.add({ domain: 'start', label: 'top', weight: 0.2 });
+  expect(rs.totalWeight(rs.domains['start'])).toBeCloseTo(2.2);
 
-  rs.add("start", "28", 0.8, ["condition"]);
-  rs.add("start", "42", 0.6, ["!condition"]);
+  rs.add({
+    domain: 'start',
+    label: '28',
+    weight: 0.8,
+    condition: ['condition']
+  });
+  rs.add({
+    domain: 'start',
+    label: '42',
+    weight: 0.6,
+    condition: ['!condition']
+  });
 
-  expect(rs.total_weight(rs.domains["start"], [])).toBeCloseTo(2.8);
-  expect(rs.total_weight(rs.domains["start"], ["condition"])).toBeCloseTo(3.0);
-  expect(rs.total_weight(rs.domains["start"], ["!condition"])).toBeCloseTo(2.8);
+  expect(rs.totalWeight(rs.domains['start'], { tags: [] })).toBeCloseTo(2.8);
+  expect(
+    rs.totalWeight(rs.domains['start'], { tags: ['condition'] })
+  ).toBeCloseTo(3.0);
+  expect(
+    rs.totalWeight(rs.domains['start'], { tags: ['!condition'] })
+  ).toBeCloseTo(2.8);
 });
 
-it("Weighted_pick is working", () => {
+it('is weightedPick working ?', () => {
   const rs = new RandomStory();
-  rs.add("start", "test");
-  rs.add("start", "end");
-  rs.add("start", "28", 0.8, ["condition"]);
-  rs.add("start", "42", 0.6, ["!condition"]);
+  rs.add({ domain: 'start', label: 'test' });
+  rs.add({ domain: 'start', label: 'end' });
+  rs.add({
+    domain: 'start',
+    label: '28',
+    weight: 0.8,
+    condition: ['condition']
+  });
+  rs.add({
+    domain: 'start',
+    label: '42',
+    weight: 0.6,
+    condition: ['!condition']
+  });
 
   rs.rand = jest.fn(() => 0.00001);
-  expect(rs.weighted_pick(rs.domains["start"]).label).toBe("test");
+  expect(rs.weightedPick(rs.domains['start']).label({})).toBe('test');
   rs.rand = jest.fn(() => 0.99999);
-  expect(rs.weighted_pick(rs.domains["start"]).label).toBe("test");
+  expect(rs.weightedPick(rs.domains['start']).label({})).toBe('test');
 
   rs.rand = jest.fn(() => 1.999999);
-  expect(rs.weighted_pick(rs.domains["start"]).label).toBe("end");
+  expect(rs.weightedPick(rs.domains['start']).label()).toBe('end');
   rs.rand = jest.fn(() => 1.00001);
-  expect(rs.weighted_pick(rs.domains["start"]).label).toBe("end");
-  expect(rs.weighted_pick(rs.domains["start"], []).label).toBe("end");
+  expect(rs.weightedPick(rs.domains['start']).label()).toBe('end');
+  expect(rs.weightedPick(rs.domains['start'], { tags: [] }).label()).toBe(
+    'end'
+  );
 
   rs.rand = jest.fn(() => 2.00001);
-  expect(rs.weighted_pick(rs.domains["start"], ["condition"]).label).toBe("28");
+  expect(
+    rs.weightedPick(rs.domains['start'], { tags: ['condition'] }).label()
+  ).toBe('28');
   rs.rand = jest.fn(() => 2.79999);
-  expect(rs.weighted_pick(rs.domains["start"], ["condition"]).label).toBe("28");
+  expect(
+    rs.weightedPick(rs.domains['start'], { tags: ['condition'] }).label()
+  ).toBe('28');
   rs.rand = jest.fn(() => 2.5999999);
-  expect(rs.weighted_pick(rs.domains["start"], ["!condition"]).label).toBe(
-    "42"
-  );
+  expect(
+    rs.weightedPick(rs.domains['start'], { tags: ['!condition'] }).label()
+  ).toBe('42');
 });
 
-it("It generate something simple", () => {
+it('It generate something simple', () => {
   const rs = new RandomStory();
-  rs.add("start", "test");
-  rs.add("start", "<toto>pouet");
-  rs.add("toto", "pouet");
-  rs.add("toto", "pit");
+  rs.add({ domain: 'start', label: 'test' });
+  rs.add({ domain: 'start', label: '<toto>pouet' });
+  rs.add({ domain: 'toto', label: 'pouet' });
+  rs.add({ domain: 'toto', label: 'pit' });
 
   rs.rand = jest.fn(() => 0.5);
-  expect(rs.resolve()).toBe("test");
+  expect(rs.resolve()).toBe('test');
 
   rs.rand = jest.fn(() => 1.5);
-  expect(rs.resolve()).toBe("pitpouet");
+  expect(rs.resolve()).toBe('pitpouet');
 });
 
-it("And something hard with condition and context addition", () => {
+it('And something hard with condition and context addition', () => {
   const rs = new RandomStory();
-  rs.add("start", "test");
-  rs.add("start", "<toto>pouet", 1, ["cond1", "!cond2"], ["add1", "!add2"]);
-  rs.add("toto", "pouet");
-  rs.add("toto", "pit", 1, ["add1"]);
+  rs.add({ domain: 'start', label: 'test' });
+  rs.add({
+    domain: 'start',
+    label: '<toto>pouet',
+    weight: 1,
+    condition: ({ tags = [] }) =>
+      tags.includes('cond1') && !tags.includes('cond2'),
+    effect: context => (context.tags = context.tags.concat(['add1', '!add2']))
+  });
+  rs.add({ domain: 'toto', label: 'pouet' });
+  rs.add({ domain: 'toto', label: 'pit', weight: 1, condition: ['add1'] });
 
   rs.rand = jest.fn((min, max) => max - 0.001);
-  expect(rs.resolve("<start>", ["cond1"])).toBe("pitpouet");
+  expect(rs.resolve('<start>', { tags: ['cond1'] })).toBe('pitpouet');
 });
 
-it("And use vars", () => {
+it('And use vars', () => {
   const rs = new RandomStory();
-  rs.add("start", "test");
-  rs.add("start", "<toto>pouet", 1, ["cond1", "!cond2"], ["add1", "!add2"]);
-  rs.add("toto", "pouet");
-  rs.add("toto", "pit$(titi)", 1, ["add1"]);
+  rs.add({ domain: 'start', label: 'test' });
+  rs.add({
+    domain: 'start',
+    label: '<toto>pouet',
+    weight: 1,
+    condition: ({ tags = [] }) =>
+      tags.includes('cond1') && !tags.includes('cond2'),
+    effect: context => (context.tags = context.tags.concat(['add1', '!add2']))
+  });
+  rs.add({ domain: 'toto', label: 'pouet' });
+  rs.add({
+    domain: 'toto',
+    label: 'pit$(titi)',
+    weight: 1,
+    condition: ({ tags }) => tags.includes('add1')
+  });
 
   rs.rand = jest.fn((min, max) => max - 0.001);
-  expect(rs.resolve("<start>", ["cond1"], { titi: "hihi" })).toBe(
-    "pithihipouet"
-  );
+  expect(
+    rs.resolve('<start>', { tags: ['cond1'], vars: { titi: 'hihi' } })
+  ).toBe('pithihipouet');
 });
